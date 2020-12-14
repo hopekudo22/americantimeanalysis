276dd03386aaec7d787678e986c88347365593a4
Hope Kudo <hopekudo@Hopes-MBP.hawaii.rr.com>
2020-12-14 11:33:15 -1000

    cleaning

--- TimeAnalysis/app.R
+++ TimeAnalysis/app.R
@@ -8,6 +8,8 @@ library(dplyr, warn.conflicts = FALSE)
 library(ggforce)
 library(gt)
 library(gtsummary)
+library(ggthemes)
+library(rstanarm)
 
 #Load ATUS data
 fulldata <- read.csv("fullset.csv")
@@ -25,12 +27,9 @@ ui <- navbarPage(
                              h3("Distribution of Hours Slept Based on Income"),
                              h4("Determining the distribution of hours slept based on family 
                      income from survey responses 2012-2016"),
-                             plotOutput("Plot1"))),
-             br(),
-             br(),
-             p("Interpretation of regression table"),
-             sidebarPanel(
-                 titlePanel("Interactive Regression:"),
+                             plotOutput("Plot1")))),
+    sidebarPanel(
+    titlePanel("Interpretation of Regression Tables"),
                  p("Select a factor to see a regression table detailing the results"),
                  selectInput(inputId = "regressiontable",
                              label = "Select Factor:",
@@ -51,7 +50,7 @@ ui <- navbarPage(
                                hours slept during the night and the factors of family income, race, sex,
                                and education level. 
                                Based on the results,
-                               I'm 95% confident that the true value of the")))),
+                               I'm 95% confident that the true value of the"))),
     
     tabPanel("State Comparison",
              fluidPage(
@@ -64,14 +63,14 @@ ui <- navbarPage(
                                                 label = "State",
                                                 choices = unique(fulldata$state), #add data set
                                                 selected = "Hawaii"),
-                                 plotOutput("Plot2")
-                 ))),
+                                 plotOutput("Plot2")))
+                 )),
              
-             tabPanel("About",
-                      fluidPage(
-                          fluidRow(column(12,
-                                          h3("Project Background"),
-                                          p("This project utilizes data from the American Time Use Survey (ATUS) that collects 
+  tabPanel("About",
+           fluidPage(
+               fluidRow(column(12,
+                               h3("Project Background"),
+                               p("This project utilizes data from the American Time Use Survey (ATUS) that collects 
                                  information on how respondents spend their time along with socioeconomic demographics.
                                  Through data visualizations and analysis, we are able to take a deeper dive into how
                                  Americans spend their time and how different variables such as income, location, or sex,
@@ -79,8 +78,8 @@ ui <- navbarPage(
                                  By looking at a wide range of activities such as how much someone sleeps in a day, the amount of housework they do, the time
                                  they spendon sporting events, and so forth. The time data can be organized by state, family income,
                                  education, age, and type of job."),
-                                          h3("Source Information"),
-                                          p("I used data from the American Time Usage Survey over the years of 2003 - 2016 from library(atus).
+                               h3("Source Information"),
+                               p("I used data from the American Time Usage Survey over the years of 2003 - 2016 from library(atus).
                                The American Time Use Survey is an annual survey that is conducted on a sample of individuals across the
                                United States and studies how individuals spend their time over the course of a day. Individual respondents
                                are interviewed about what activities they engaged in, the time duration spent on those activities, at what
@@ -90,86 +89,85 @@ ui <- navbarPage(
                                that comprise of time spent sleeping, on housework, volunteering, playing sports, practicing religion, eating
                                and drinking, shopping, research and homework, time in class, watching television, drug usage, and computer usage.
                                  thus, the data is from the years 2012-2017."),
-                                          h3("About Me"),
-                                          p("My name is Hope Kudo and I'm a junior at Harvard studying Government and Psychology. As a proud
+                               h3("About Me"),
+                               p("My name is Hope Kudo and I'm a junior at Harvard studying Government and Psychology. As a proud
                                member of the Class of 2022 and Quincy House, living at home this semester has been quite a shake up,
                                but this project has been a great to delve into interesting data and a great way to experiment with R!
                                I'm excited to put the skills I've learned this semester into use. I can be reached at hopekudo@college.harvard.edu. 
-                               The code for this project can be found at my", a("GitHub page here.", href = "https://github.com/hopekud22/50proj"))))
+                               The code for this project can be found at my", a("GitHub page here.", href = "https://github.com/hopekud22/50proj"))
                       )
              )
-    ),
+    )))
     
+#Define server logic
     
+server <- function(input, output, session) {
+        
+    output$Plot1 <- renderPlot({
+        fulldata %>%
+            ggplot(aes(x = sleep, fill = famincome)) +
+            geom_histogram() +
+            facet_wrap(~ famincome) +
+            theme(axis.text = element_text(size = 5), strip.text = element_text(size = 7),
+                  panel.grid = element_blank(), panel.spacing.x = unit(3, "mm"),
+                  axis.ticks = element_blank(), axis.ticks.y = element_blank()) +
+            scale_fill_discrete(name = "Family Income") +
+            labs(title = "Distribution of Hours Slept Based on Income",
+                 subtitle = "Determining the distribution of hours slept based on family income from survey responses 2012-2016",
+                 x = "Hours Slept",
+                 y = "",
+                 caption = "Source: ATUS data") +
+            theme_linedraw()
+    })
+
+    output$sleepincome <- renderPlot({
+        sleepincome <- data %>%
+            ggplot(aes(x = sleep, y = famincome)) +
+            geom_boxplot() +
+            theme_bw() +
+            labs(x = "Hours of Sleep",
+                 y = "Family Income ($)",
+                 title = "Distribution of Hours of Sleep Linked to Income")
+        
+        sleepincome
+    })
+        
+
+    output$sleeprace <- renderPlot ({
+        sleeprace <- data %>%
+            ggplot(aes(x = sleep, y = race)) +
+            geom_boxplot() +
+            theme_bw() +
+            labs(x = "Hours of Sleep",
+                 y = "Race",
+                 title = "Distribution of Hours of Sleep Linked to Race")
+        
+        sleeprace
+    })
     
-    # Define server logic
-    
-    server <- function(input, output, session) {
+    output$sleepgender <- renderPlot({
+        sleepgender <- data %>%
+            ggplot(aes(x = sleep, y = sex)) +
+            geom_boxplot() +
+            theme_bw() +
+            labs(x = "Hours of Sleep",
+                 y = "Sex",
+                 title = "Distribution of Hours of Sleep Linked to Sex")
         
-        output$Plot1 <- renderPlot({
-            fulldata %>%
-                ggplot(aes(x = sleep, fill = famincome)) +
-                geom_histogram() +
-                facet_wrap(~ famincome) +
-                theme(axis.text = element_text(size = 5), strip.text = element_text(size = 7),
-                      panel.grid = element_blank(), panel.spacing.x = unit(3, "mm"),
-                      axis.ticks = element_blank(), axis.ticks.y = element_blank()) +
-                scale_fill_discrete(name = "Family Income") +
-                labs(title = "Distribution of Hours Slept Based on Income",
-                     subtitle = "Determining the distribution of hours slept based on family income from survey responses 2012-2016",
-                     x = "Hours Slept",
-                     y = "",
-                     caption = "Source: ATUS data") +
-                theme_linedraw()
-        })
+        sleepgender
+    })
         
-        output$sleepincome <- renderPlot({
-            sleepincome <- data %>%
-                ggplot(aes(x = sleep, y = famincome)) +
-                geom_boxplot() +
-                theme_bw() +
-                labs(x = "Hours of Sleep",
-                     y = "Family Income ($)",
-                     title = "Distribution of Hours of Sleep Linked to Income")
-            
-            sleepincome
-        })
+    output$sleepedu <- renderPlot ({
+        sleepedu <- data %>%
+            ggplot(aes(x = sleep, y = edu)) +
+            geom_boxplot() +
+            theme_bw() +
+            labs(x = "Hours of Sleep",
+                 y = "Level of Education",
+                 title = "Distribution of Hours of Sleep Linked to Level of Education")
         
-        output$sleeprace <- renderPlot ({
-            sleeprace <- data %>%
-                ggplot(aes(x = sleep, y = race)) +
-                geom_boxplot() +
-                theme_bw() +
-                labs(x = "Hours of Sleep",
-                     y = "Race",
-                     title = "Distribution of Hours of Sleep Linked to Race")
-            
-            sleeprace
-        })
-        
-        output$sleepgender <- renderPlot({
-            sleepgender <- data %>%
-                ggplot(aes(x = sleep, y = sex)) +
-                geom_boxplot() +
-                theme_bw() +
-                labs(x = "Hours of Sleep",
-                     y = "Sex",
-                     title = "Distribution of Hours of Sleep Linked to Sex")
-            
-            sleepgender
-        })
-        
-        output$sleepedu <- renderPlot ({
-            sleepedu <- data %>%
-                ggplot(aes(x = sleep, y = edu)) +
-                geom_boxplot() +
-                theme_bw() +
-                labs(x = "Hours of Sleep",
-                     y = "Level of Education",
-                     title = "Distribution of Hours of Sleep Linked to Level of Education")
-            
-            sleepedu
-        })
+        sleepedu
+    })
         
         regressionInput <- reactive ({
             switch(input$regression,
@@ -180,16 +178,17 @@ ui <- navbarPage(
             
         })
         
-        output$regression <- render_gt({
-            formula <- regressionInput()
+        output$regressiontable <- render_gt({
+            formula <- regressiontableInput()
             set.seed(100)
             fit_obj <- stan_glm(formula,
-                                data = data,
+                                data = fulldata,
+                                family = gaussian(),
                                 refresh = 0) 
             fit_obj %>%
                 tidy() %>%
                 tbl_regression() %>%
-                gt() %>%
+                as_gt() %>%
                 tab_header(title = "Regression of Factors Impact on Sleep") %>% 
                 tab_source_note("Source: ATUS data") 
             
